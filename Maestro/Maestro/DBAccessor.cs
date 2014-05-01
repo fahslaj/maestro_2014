b4b2400af59ea5fd0933763a5f694c1cfcec0f84
redacted-moose <sadlercr@rose-hulman.edu>
2014-04-30 22:46:50 -0400

    Delete and Update

--- Maestro/Maestro/DBAccessor.cs
+++ Maestro/Maestro/DBAccessor.cs
@@ -10,6 +10,7 @@ namespace Maestro
 {
     static class DBAccessor
     {
+        static string connString = @"Persist Security Info=False; User ID=joe;Initial Catalog=Inventory;Password=shmoe;Data Source=bogus.network.name";
         public DataTable selectAllTable(String name)
         {
             String query = "SELECT * FROM " + name;
@@ -33,14 +34,30 @@ namespace Maestro
 
         }
 
-        public DataTable updateEntry(String attributes)
+        public DataTable updateEntry(String tablename, DataRow oldrow, String attributes)
         {
-
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+            {
+                string sqlQuery = @"SELECT * from Items";
+                SqlCommand cmd = new SqlCommand(sqlQuery, sqlConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                da.Fill(table);
+                dataGridView1.DataSource = new BindingSource(table, null);
+            }
         }
 
         public DataTable deleteEntry(String attributes)
         {
-
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+            {
+                string sqlQuery = @"DELETE * from Items";
+                SqlCommand cmd = new SqlCommand(sqlQuery, sqlConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                da.Fill(table);
+                dataGridView1.DataSource = new BindingSource(table, null);
+            }
         }
     }
 }
