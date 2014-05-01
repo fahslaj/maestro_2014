bc623c24a541ddbd6cbea16dfb304fd973b5b532
royappmj <royappmj@rose-hulman.edu>
2014-04-30 22:29:48 -0400

    added methods

--- Maestro/Maestro/DBAccessor.cs
+++ Maestro/Maestro/DBAccessor.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+//using System
 
 namespace Maestro
 {
     static class DBAccessor
     {
+        public DataTable selectAllTable(String name)
+        {
+            String query = "SELECT * FROM " + name;
 
+            // create to strings for the connection and the query
+            string connString = @"Persist Security Info=False; User ID=joe;Initial Catalog=Inventory;Password=inventory;Data Source=vmspdgdr001.amr.corp.intel.com";
+
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+            {
+                string sqlQuery = @"SELECT * from Items";
+                SqlCommand cmd = new SqlCommand(sqlQuery, sqlConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                da.Fill(table);
+                dataGridView1.DataSource = new BindingSource(table, null);
+            }
+        }
+
+        public DataTable insertEntry(String attributes)
+        {
+
+        }
+
+        public DataTable updateEntry(String attributes)
+        {
+
+        }
+
+        public DataTable deleteEntry(String attributes)
+        {
+
+        }
     }
 }
