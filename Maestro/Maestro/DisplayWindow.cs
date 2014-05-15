30e5f885d31f161f5004408630a04ddebfba03d5
redacted-moose <sadlercr@rose-hulman.edu>
2014-05-15 00:37:46 -0400

    Login and Register stuff

--- Maestro/Maestro/DisplayWindow.cs
+++ Maestro/Maestro/DisplayWindow.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Renci.SshNet;
 
 namespace Maestro
 {
@@ -19,8 +20,9 @@ namespace Maestro
         {
             InitializeComponent();
             this.Text = "Maestro: Guest User";
-            byte[] address = {137,112,128,188};
-            streamer = new MediaStreamer(new System.Net.IPAddress(address), 6600, 8000);
+//            byte[] address = { 137, 112, 128, 188 };
+//            streamer = new MediaStreamer(new System.Net.IPAddress(address), 6600, 8000);
+            
 //            streamer.Play();
         }
 
@@ -38,12 +40,36 @@ namespace Maestro
             LoginWindow lw = new LoginWindow();
             lw.ShowDialog();
             this.Text = "Maestro: Logged in as "+lw.LoggedIn;
+            SshClient ssh = new SshClient("137.112.128.188", "mpd", "mpd");
+            ssh.Connect();
+            SshCommand cmd1 = ssh.CreateCommand("cat userconfs/" + lw.LoggedIn + ".conf");
+            String output = cmd1.Execute();
+            int index = output.IndexOf("port");
+            String port = output.Substring(index + 6, 4);
+            int portnum = int.Parse(port);
+            SshCommand command = ssh.CreateCommand("mpd userconfs/" + lw.LoggedIn + ".conf &");
+            command.Execute();
+            ssh.Disconnect();
+            byte[] address = { 137, 112, 128, 188 };
+            streamer = new MediaStreamer(new System.Net.IPAddress(address), 6600, portnum);
         }
 
         private void RegisterButton_Click(object sender, EventArgs e)
         {
             RegisterWindow rw = new RegisterWindow();
             rw.ShowDialog();
+            SshClient ssh = new SshClient("137.112.128.188", "mpd", "mpd");
+            ssh.Connect();
+            SshCommand cmd1 = ssh.CreateCommand("cat port");
+            String portnum = cmd1.Execute();
+            SshCommand command = ssh.CreateCommand("echo 'user \"mpd\"\nrestore_paused \"yes\"\npid_file \"/run/mpd/mpd.pid\"\ndb_file \"/var/lib/mpd/mpd.db\"\nstate_file \"/var/lib/mpd/mpdstate\"\nplaylist_directory \"/var/lib/mpd/playlists\"\nmusic_directory \"/var/lib/mpd/music\"\naudio_output {\n\ttype\t\"httpd\"\n\tbind_to_address\t\"137.112.128.188\"\n\tname\t\"My HTTP Stream\"\n\tencoder\t\"lame\"\n\tport\t\"" + portnum + "\"\n\tbitrate\t\"320\"\n\tformat\t\"44100:16:1\"\n}' > userconfs/" + rw.username + ".conf");
+            command.Execute();
+            int portnumnum = int.Parse(portnum);
+            //StreamPort = portnumnum;
+            portnumnum++;
+            SshCommand cmd2 = ssh.CreateCommand("echo \"" + portnumnum + "\" > port");
+            cmd2.Execute();
+            ssh.Disconnect();
         }
 
         private void AddEntryButton_Click(object sender, EventArgs e)
