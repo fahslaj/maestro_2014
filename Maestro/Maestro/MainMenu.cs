b0f89589ec0b42d15fc804ff4d931b3a0262941b
fahslaj <fahslaj@rose-hulman.edu>
2014-05-19 13:58:54 -0400

    Main Menu?

---
+++ Maestro/Maestro/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maestro
+{
+    class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.ClientSize = new System.Drawing.Size(458, 668);
+            this.Name = "MainMenu";
+            this.Text = "Maestro";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
